621202af782a307250a89dec3112f62c8f4681c4
j1msh <118993878+j1msh@users.noreply.github.com>
2022-11-25 00:07:18 -0500

    Add files via upload

---
+++ codes/task_1.ino
@@ -0,0 +1,64 @@
+#include <IRremote.h>
+
+const int RECV_PIN = 7;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+unsigned long key_value = 0;
+int car_dir;
+
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+}
+
+void loop(){
+  if (irrecv.decode(&results)){ 
+
+        switch(results.value){
+          case 0xFF629D:
+          forward();
+          Serial.println("FORWARD");
+          break;
+          case 0xFF22DD:
+          right();
+          Serial.println("RIGHT");
+          break;
+          case 0xFFC23D:
+          left();
+          Serial.println("LEFT");
+          break;
+          case 0xFFE21D:
+          stop();
+          Serial.println("STOP");
+          break ;
+        }
+        irrecv.resume(); 
+  }
+}
+
+void forward(){
+  analogWrite(11,255);
+  analogWrite(12,255); 
+}
+
+
+void left()
+{
+  car_dir = 2;
+  analogWrite(11,0);
+  analogWrite(12,255);
+}
+
+void right()
+{
+  car_dir = 3;
+  analogWrite(11,255);
+  analogWrite(12,0);
+}
+
+void stop()
+{
+  analogWrite(11,0);
+  analogWrite(12,0);
+}
